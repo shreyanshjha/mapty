ee1118339ecce7b26d29ab3465a8eca292adcf91
shreyanshjha <shreyanshjha22@gmail.com>
2025-01-18 20:27:15 +0530

    refactoring code using classes and methods

--- script.js
+++ script.js
@@ -10,3 +10,68 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+class App {
+    #map;
+    #mapEvent;
+    constructor() {
+        this._getPosition();
+
+        form.addEventListener('submit', this._newWorkout.bind(this));
+
+        inputType.addEventListener('change', this._toggleElevationField);
+    }
+
+    _getPosition() {
+        navigator.geolocation.getCurrentPosition(this._loadMap.bind(this), function(){
+            alert('Could not get your current location');
+        });
+    }
+
+    _loadMap(position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const coords = [latitude, longitude];
+        this.#map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.#map);
+
+        this.#map.on('click', this._showForm.bind(this));
+    }
+
+    _showForm(mapE) {
+        this.#mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+
+    _toggleElevationField() {
+        inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+        inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+
+    _newWorkout(e) {
+        e.preventDefault();
+
+        const {lat, lng} = this.#mapEvent.latlng;
+
+        L.marker([lat, lng]).addTo(this.#map)
+            .bindPopup(L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+            .setPopupContent('Workout')
+            .openPopup();
+
+        // Clear input values
+        inputDistance.value = inputDuration.value = inputCadence.value = inputElevation.value = "";
+    }
+}
+
+const app = new App();
+
